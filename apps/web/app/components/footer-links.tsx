46b7770bbd49e085c5ba1d98501b5d655b84996f
laitman <47023817+LaitmanX@users.noreply.github.com>
2025-01-21 17:12:16 +0100

    feat: footer links

---
+++ apps/web/app/components/footer-links.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export const Links = () => (
+  <div className="grid grid-cols-3 gap-1 w-[400px]">
+    <Link
+      href={`${process.env.NEXT_PUBLIC_API_URL}/docs`}
+      className="bg-[#27d17f] hover:bg-[#146940]/90 text-black px-4 py-2 font-semibold rounded-lg  text-center"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      API Docs
+    </Link>
+    <Link
+      href="https://github.com/dimlaitman/bitpanda-demo"
+      className="bg-[#27d17f] hover:bg-[#146940]/90 text-black px-4 py-2 font-semibold rounded-lg text-center"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Github Repo
+    </Link>
+    <Link
+      href="https://www.linkedin.com/in/laitman/"
+      className="bg-[#27d17f] hover:bg-[#146940]/90 text-black px-4 py-2 font-semibold rounded-lg text-center"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      LinkedIn
+    </Link>
+  </div>
+);
--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { PrimitiveMiddleware } from "./middleware";
+import { Links } from "./components/footer-links";
 
 export const metadata: Metadata = {
   title: "BitPanda demo | DeFi",
@@ -16,12 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-screen w-screen overflow-hidden">
-      <body className="flex items-center justify-center h-full w-full bg-black">
+      <body className="flex items-center justify-center h-full w-full bg-black flex-col space-y-2">
         <div className="h-[700px] w-[400px]  rounded-xl shadow-lg overflow-hidden outline  outline-1 outline-zinc-500">
           <Providers>
             <PrimitiveMiddleware>{children}</PrimitiveMiddleware>
           </Providers>
         </div>
+        <Links />
       </body>
     </html>
   );
