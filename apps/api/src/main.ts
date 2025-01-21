c7eaa92619e01651709c73da22fe69d037a569f8
laitman <47023817+LaitmanX@users.noreply.github.com>
2025-01-21 15:58:36 +0100

    feat: swagger ui

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,9 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { generateOpenApi } from '@ts-rest/open-api';
+import { contract } from '@bitpanda-demo/contracts';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +12,14 @@ async function bootstrap() {
     origin: '*',
   });
 
+  const document = generateOpenApi(contract, {
+    info: {
+      title: 'Bitpanda Demo | API',
+      version: '0.0.1',
+    },
+  });
+
+  SwaggerModule.setup('docs', app, document);
   const port = process.env.PORT || 8080;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}`);
